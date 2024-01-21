91c021388374f618905772cf06395d813abae29e
MohammedKevin <kevin.mohammed16@gmail.com>
2024-01-21 18:04:57 +0100

    change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
       <div className="main-content">
         <p>Kunst Archiv - Sachendingekonzepte ##########</p>
         <div className="white-block"></div>
-        <p style={{ textAlign: "right" }}>mohammed kevin </p>
+
+        <p style={{ textAlign: "right", marginRight: "10vw" }}>
+          mohammed kevin
+        </p>
 
         <div className="main-images">
           <img src={`/images/kind.png`} alt={`Kind`} className="image-kind" />
