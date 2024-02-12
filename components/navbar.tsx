9757228ceb8621d1d8de2eaf0e99d5f22e57f0ce
MohammedKevin <kevin.mohammed16@gmail.com>
2024-02-12 10:56:43 +0100

    feat: add top banner

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,7 @@ const Navbar = () => {
   const linkStyle = {
     color: "black", // Set the desired text color
     textDecoration: "none", // Remove underline
-    fontSize: '35px',
-    paddingTop: '100px',
+    fontSize: "35px",
   };
 
   return (
