408f3c14fd7937c35e736368b26694ca59a0ef02
MohammedKevin <kevin.mohammed16@gmail.com>
2024-03-07 14:03:50 +0100

    feat: woerter

---
+++ app/woerter/page.tsx
@@ -0,0 +1,20 @@
+import ImageGallery from "../../components/ImageGallery";
+
+export default function Bilder() {
+  const pageStyles = {
+    backgroundColor: "#white", // Replace with your preferred color
+    // Add other styles as needed
+  };
+
+  return (
+    <>
+      <div className="main-content" style={pageStyles}>
+        <p>Kevins Archiv - Sachendingekonzepte ##########</p>
+        <p>Woerter</p>
+        <a href={"https://www.instagram.com/woerterkollektiv/"}>
+          https://www.instagram.com/woerterkollektiv/
+        </a>
+      </div>
+    </>
+  );
+}
